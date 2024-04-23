58d9e0379d2a73ee1dec975062157e0566be1731
Poyang Huang <124699019+Davidhuang616@users.noreply.github.com>
2024-04-23 16:21:59 +0200

    Update button_listener.py

--- button_listener.py
+++ button_listener.py
@@ -1,18 +1,18 @@
-from selenium import webdriver
+xfrom selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from pynput import keyboard
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')
-driver.get("http://localhost:8000/display.html")
+driver.get("http://localhost:8000/index.html")
 
 def on_press(key):
     if key == keyboard.Key.space:
-        driver.get("http://localhost:8000/display.html#start")
+        driver.get("http://localhost:8000/index.html#start")
 
 def on_release(key):
     if key == keyboard.Key.space:
-        driver.get("http://localhost:8000/display.html#stop")
+        driver.get("http://localhost:8000/index.html#stop")
         # Stop listener
         return False
 
